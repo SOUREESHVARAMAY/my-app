991afca83b5b66f0f914687d7c09cb618e6ed6ce
SOUREESHVARAMAY <syarakaraju@my.okcu.edu>
2024-10-30 15:54:20 -0500

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,55 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
-function App() {
+function Square({ value, onSquareClick }) {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <button className="square" onClick={onSquareClick}>
+      {value}
+    </button>
+  );
+}
+
+function App() {
+  const [squares, setSquares] = useState(Array(9).fill(null));
+  const [isXNext, setIsXNext] = useState(true);
+
+  function handleClick(index) {
+    if (squares[index]) return; 
+
+    const nextSquares = squares.slice();
+    nextSquares[index] = isXNext ? 'X' : 'O';
+    setSquares(nextSquares);
+    setIsXNext(!isXNext); 
+  }
+
+  function renderSquare(index) {
+    return (
+      <Square
+        value={squares[index]}
+        onSquareClick={() => handleClick(index)}
+      />
+    );
+  }
+
+  return (
+    <div className="container">
+    <div className="board-row">
+      {renderSquare(0)}
+      {renderSquare(1)}
+      {renderSquare(2)}
     </div>
+    <div className="board-row">
+      {renderSquare(3)}
+      {renderSquare(4)}
+      {renderSquare(5)}
+    </div>
+    <div className="board-row">
+      {renderSquare(6)}
+      {renderSquare(7)}
+      {renderSquare(8)}
+    </div>
+  </div>
+
   );
 }
 
